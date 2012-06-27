4c758ed1e982c1663106653bbd80e1ce71a5cf8a
rabisg <guha.rabishankar@gmail.com>
2012-06-27 15:23:13 +0530

    Renamed Module

--- js/status.js
+++ js/status.js
@@ -2,25 +2,25 @@
   var maxlength = 140;
   Drupal.behaviors.insight_publish_module = {
     attach: function (context, settings) {
-      $('#edit-insight-publish-characters').hide();
-      $('#edit-insight-publish-textfield', context).bind('keyup keydown', function () {
-        if($('#edit-insight-publish-twitter').is(":checked") && $(this).val().length > maxlength) {
+      $('#edit-insight-status-characters').hide();
+      $('#edit-insight-status-textfield', context).bind('keyup keydown', function () {
+        if($('#edit-insight-status-twitter').is(":checked") && $(this).val().length > maxlength) {
           $(this).val($(this).val().substring(0, maxlength)); 
         }
-        $("label[for='edit-insight-publish-characters']").text(maxlength-$(this).val().length + ' characters left');
+        $("label[for='edit-insight-status-characters']").text(maxlength-$(this).val().length + ' characters left');
       });
-      $('#edit-insight-publish-twitter', context).click( function () {
-        $('#edit-insight-publish-characters').toggle();
-        $('#edit-insight-publish-textfield').trigger('keydown');          
+      $('#edit-insight-status-twitter', context).click( function () {
+        $('#edit-insight-status-characters').toggle();
+        $('#edit-insight-status-textfield').trigger('keydown');          
       });
     }
   };
 })(jQuery);
 
 function confirmPost() {
-  if(!(document.getElementById('edit-insight-publish-textfield').value.length > 0))
+  if(!(document.getElementById('edit-insight-status-textfield').value.length > 0))
     return false;
-  if(document.getElementById('edit-insight-publish-twitter').checked)
+  if(document.getElementById('edit-insight-status-twitter').checked)
     return confirm('Are you sure you want to post this?');
   return true;
 }
